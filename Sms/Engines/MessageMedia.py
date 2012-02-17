7c94d3e2eb7f29999349dfe6522a12927ec24079
Michal Ludvig <mludvig@logix.net.nz>
2012-02-17 17:15:32 +1300

    Support MessageMedia.sendMulti()

--- Sms/Engines/MessageMedia.py
+++ Sms/Engines/MessageMedia.py
@@ -12,7 +12,7 @@ from suds.client import Client
 class SmsDriver(GenericSoap.SmsDriver):
     url = 'http://soap.m4u.com.au/?wsdl'
 
-    def send(self, message, recipient):
+    def sendMulti(self, message, recipients):
         client = Client(self.url, cache = self.cache)
 
         # Authentication
@@ -20,16 +20,14 @@ class SmsDriver(GenericSoap.SmsDriver):
         auth.userId = self.options['username']
         auth.password = self.options['password']
 
-        # Message ID
-        message_id = random.randint(100000000, 999999999)
-
         # Message Type
         message_t = client.factory.create("MessageType")
         message_t.content = message
         message_t.deliveryReport = True
-        message_t.recipients.recipient.append(client.factory.create("RecipientType"))
-        message_t.recipients.recipient[-1].value = recipient
-        message_t.recipients.recipient[-1]._uid = message_id
+        for recipient in recipients:
+            message_t.recipients.recipient.append(client.factory.create("RecipientType"))
+            message_t.recipients.recipient[-1].value = recipient
+            message_t.recipients.recipient[-1]._uid = random.randint(100000000, 999999999)
 
         # Container for multiple messages
         send_messages_t = client.factory.create("SendMessagesBodyType")
@@ -38,10 +36,14 @@ class SmsDriver(GenericSoap.SmsDriver):
         # Send it out
         ret = client.service.sendMessages(auth, send_messages_t)
 
-        if ret._sent > 0:
-            info("SMS(MessageMedia) sent to %s with ID: %s" % (recipient, message_id))
-            return message_id
-        if ret._failed > 0:
-            warning("SMS(MessageMedia) failed to %s: %s" % (recipient, ret.errors.error[0]._code))
+        error_recipients = []
+        for error in ret.errors.error:
+            for recipient in error.recipients.recipient:
+                warning("SMS(MessageMedia) failed to %s: %s" % (recipient.value, error._code))
+                error_recipients.append(recipient.value)
 
-# vim:et:ts=4:sts=4:ai
+        for recipient in message_t.recipients.recipient:
+            if recipient.value not in error_recipients:
+                info("SMS(MessageMedia) sent to %s with ID: %s" % (recipient.value, recipient._uid))
+
+# vim:et:sw=4:sts=4:ai:sta
